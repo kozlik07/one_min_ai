b53d9a64fa22245514a178e998f8a3842ce3f20a
kozlik07 <45555966+kozlik07@users.noreply.github.com>
2025-03-04 09:46:25 +0100

    Update init.py

--- custom_components/one_min_ai/init.py
+++ custom_components/one_min_ai/init.py
@@ -10,3 +10,17 @@ async def async_setup(hass, config):
         hass.helpers.discovery.async_load_platform("sensor", DOMAIN, {}, config)
     )
     return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up 1min AI from a config entry."""
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    hass.data[DOMAIN].pop(config_entry.entry_id)
+    return True
